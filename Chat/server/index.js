0e34017c63f4ec9be2de222e478c29f6d1befacf
fachrizahidaya <fachriza.theblues@gmail.com>
2023-02-19 02:31:45 +0700

    updated

--- Chat/server/index.js
+++ Chat/server/index.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const PORT = 9000
-const db = require("../models")
-const bearerToken = require('express-bearer-token')
-const path = require('path')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const PORT = 9000;
+const db = require("./models");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.use(express.json())
-app.use(cors())
-app.use(bearerToken())
+app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, (err) => {
-    if (err) {
-      console.log(`ERROR: ${err}`);
-    } else {
-      // db.sequelize.sync({ alter: true });
-      console.log(`APP RUNNING at ${PORT} ✅`);
-    }
-  });
+server.listen(3001, () => {
+  console.log("Server running")
+});
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(`ERROR: ${err}`);
+//   } else {
+//     // db.sequelize.sync({ alter: true });
+//     console.log(`APP RUNNING at ${PORT} ✅`);
+//   }
+// });
